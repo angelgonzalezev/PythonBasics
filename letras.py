d3609e299979345c6aa2f379dfe615262894219b
Ángel González <aangel.gg@hotmail.com>
2020-08-12 12:27:49 +0200

    Add files via upload

---
+++ letras.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+import turtle
+
+
+
+#def write_lette(letter):
+    #if letter=='a' or letter=='A':
+    #    turtle.left(45)
+    #    turtle.forward(100)
+    #    turtle.right(90)
+    #    turtle.forward(90)
+    #else:
+    #    turte.fd(1)
+
+
+def run():
+    letter=str(input('Escribe una letra, se escribirá en mayuscula')
+    window=turtle.Screen()
+    ##write_lette(letter):
+    turtle.forward(1)
+turtle.mainloop()
+
+int __name__=='__main__'
+    run()
